eb8a34a3b11b7d1f8292e2ad5405ad3140214450
unknown <ehabtalaat5552@gmail.com>
2023-05-24 23:55:05 +0300

    Add

--- resources/views/layouts/admin_includes/sidebar.blade.php
+++ resources/views/layouts/admin_includes/sidebar.blade.php
@@ -77,6 +77,11 @@
 				 				<span class="menu-text">{{__('messages.roles')}}</span>
 							</a> 
 						</li>
+						<li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
+							<a href="{{route('website_texts.index')}}" class="menu-link">
+				 				<span class="menu-text">{{__('messages.website_texts')}}</span>
+							</a> 
+						</li>
 
 					</ul>
 				</div>
